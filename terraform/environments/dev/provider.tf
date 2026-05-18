18b997ba434058f19cec30216d5d1559adc98a4c
cakefeelsgood <kimminji15441544@gmail.com>
2026-05-18 15:40:09 +0900

    chore: Terraform S3 backend 및 state lock 구성 (#301)

--- terraform/environments/dev/provider.tf
+++ terraform/environments/dev/provider.tf
@@ -8,13 +8,16 @@ terraform {
     }
   }
 
+
   backend "s3" {
-    # bucket         = "moment-terraform-state-<account-id>"
-    # key            = "dev/terraform.tfstate"
-    # region         = "ap-northeast-3"
-    # dynamodb_table = "moment-terraform-lock"
-    # encrypt        = true
+    bucket         = "moment-team04-dev-terraform-state-611058323802-ap-northeast-3"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-northeast-3"
+    encrypt        = true
+    use_lockfile   = true
+    dynamodb_table = "moment-team04-dev-terraform-lock"
   }
+
 }
 
 # Primary Region: ap-northeast-3 (Osaka)
