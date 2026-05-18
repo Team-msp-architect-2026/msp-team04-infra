ac5682fdf6c1da22c4f37677f7b38e97d87809b5
armddi <jungarum0607@gmail.com>
2026-05-18 20:31:09 +0900

    chore: remove terraform plan file and add app vpc module changes

---
+++ terraform/modules/app-vpc/main.tf
@@ -0,0 +1,126 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-vpc"
+  })
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-igw"
+  })
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(var.tags, {
+    Name                     = "${var.name_prefix}-public-${count.index + 1}"
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
+  })
+}
+
+resource "aws_subnet" "private_app" {
+  count = length(var.private_app_subnet_cidrs)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_app_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = merge(var.tags, {
+    Name                              = "${var.name_prefix}-private-app-${count.index + 1}"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
+  })
+}
+
+resource "aws_subnet" "tgw_attachment" {
+  count = length(var.tgw_attachment_subnet_cidrs)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.tgw_attachment_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-tgw-attach-${count.index + 1}"
+  })
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-public-rt"
+  })
+}
+
+resource "aws_route" "public_default_to_igw" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
+  count = var.transit_gateway_id != "" ? 1 : 0
+
+  subnet_ids         = aws_subnet.tgw_attachment[*].id
+  transit_gateway_id = var.transit_gateway_id
+  vpc_id             = aws_vpc.this.id
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-tgw-attachment"
+  })
+}
+
+resource "aws_route_table" "private_app" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-private-app-rt"
+  })
+}
+
+resource "aws_route" "private_default_to_tgw" {
+  count = var.transit_gateway_id != "" ? 1 : 0
+
+  route_table_id         = aws_route_table.private_app.id
+  destination_cidr_block = "0.0.0.0/0"
+  transit_gateway_id     = var.transit_gateway_id
+
+  depends_on = [aws_ec2_transit_gateway_vpc_attachment.this]
+}
+
+resource "aws_route" "private_data_vpc_to_tgw" {
+  count = var.transit_gateway_id != "" ? 1 : 0
+
+  route_table_id         = aws_route_table.private_app.id
+  destination_cidr_block = var.data_vpc_cidr
+  transit_gateway_id     = var.transit_gateway_id
+
+  depends_on = [aws_ec2_transit_gateway_vpc_attachment.this]
+}
+
+resource "aws_route_table_association" "private_app" {
+  count = length(aws_subnet.private_app)
+
+  subnet_id      = aws_subnet.private_app[count.index].id
+  route_table_id = aws_route_table.private_app.id
+}
