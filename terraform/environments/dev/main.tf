1a0ad6acd1c86b03e4001f75e6bd0aec0579a000
armddi <jungarum0607@gmail.com>
2026-05-22 12:45:25 +0900

    [M2-NET-03] Configure Prod VPC subnet tiers

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -59,3 +59,42 @@ module "network_vpc" {
 
   tags = local.common_tags
 }
+
+module "prod_vpc" {
+  source = "../../modules/prod-vpc"
+
+  project_name = var.project_name
+  env          = "prod"
+
+  vpc_cidr = "10.10.0.0/16"
+
+  availability_zones = [
+    "${var.primary_region}a",
+    "${var.primary_region}c"
+  ]
+
+  public_subnet_cidrs = [
+    "10.10.0.0/24",
+    "10.10.1.0/24"
+  ]
+
+  private_app_subnet_cidrs = [
+    "10.10.10.0/24",
+    "10.10.11.0/24"
+  ]
+
+  private_data_subnet_cidrs = [
+    "10.10.20.0/24",
+    "10.10.21.0/24"
+  ]
+
+  tgw_subnet_cidrs = [
+    "10.10.100.0/28",
+    "10.10.100.16/28"
+  ]
+
+  # TGW 생성 후 후속 이슈에서 연결
+  transit_gateway_id = null
+
+  tags = local.common_tags
+}
--- terraform/modules/prod-vpc/variables.tf
+++ terraform/modules/prod-vpc/variables.tf
@@ -1,51 +1,51 @@
-variable "name_prefix" {
-  description = "리소스 이름 prefix"
+variable "project_name" {
+  description = "Project name."
+  type        = string
+}
+
+variable "env" {
+  description = "Workload environment name. Example: prod."
   type        = string
 }
 
 variable "vpc_cidr" {
-  description = "App VPC CIDR"
+  description = "Prod VPC CIDR block."
   type        = string
 }
 
 variable "availability_zones" {
-  description = "사용할 AZ 목록"
+  description = "Availability zones for subnet creation."
   type        = list(string)
 }
 
 variable "public_subnet_cidrs" {
-  description = "Public Subnet CIDR 목록"
+  description = "Public subnet CIDR blocks for internet-facing ALB."
   type        = list(string)
 }
 
 variable "private_app_subnet_cidrs" {
-  description = "Private App Subnet CIDR 목록"
+  description = "Private app subnet CIDR blocks for EKS worker nodes and pods."
   type        = list(string)
 }
 
-variable "tgw_attachment_subnet_cidrs" {
-  description = "TGW Attachment Subnet CIDR 목록"
+variable "private_data_subnet_cidrs" {
+  description = "Private data subnet CIDR blocks for RDS, Redis, and OpenSearch."
+  type        = list(string)
+}
+
+variable "tgw_subnet_cidrs" {
+  description = "TGW attachment subnet CIDR blocks."
   type        = list(string)
 }
 
 variable "transit_gateway_id" {
-  description = "Transit Gateway ID"
+  description = "Transit Gateway ID. If null, TGW routes are not created yet."
   type        = string
-  default     = ""
-}
-
-variable "data_vpc_cidr" {
-  description = "Data VPC CIDR"
-  type        = string
-}
-
-variable "eks_cluster_name" {
-  description = "EKS Cluster Name"
-  type        = string
+  default     = null
 }
 
 variable "tags" {
-  description = "공통 태그"
+  description = "Common tags."
   type        = map(string)
   default     = {}
 }
